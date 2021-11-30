536ee97c7b13479182ae7cf0abca0013d2a639d7
Samet Can KARAKAŞ <scankarakas@hotmail.com>
2021-11-30 18:06:03 +0300

    used all_movies[::-1] way to renumarate list from 1 .

--- main.py
+++ main.py
@@ -10,11 +10,22 @@ soup = BeautifulSoup(empire_web_site, "html.parser")
 # print(soup.text)
 
 movie_title = soup.find_all("h3", class_="title")
-top100_list = []
-for num in range(len(movie_title)-1, -1, -1):
-    print(num)
-    with open("movie.txt", "a", encoding="UTF-8") as movie_list:
-        movie_list.write(movie_title[num].text + "\n")
-    # top100_list.append(movie_title[num].text
 
-print(top100_list)
+
+all_movies = [movie.getText() for movie in movie_title]
+movies = all_movies[::-1]
+with open("movie.txt", "w", encoding="UTF-8") as movie_file:
+    for movie in movies:
+        movie_file.writelines(f"{movie}\n")
+
+
+
+
+
+
+
+# for num in range(len(movie_title)-1, -1, -1):
+#     print(num)
+#     with open("movie.txt", "a", encoding="UTF-8") as movie_list:
+#         movie_list.write(movie_title[num].text + "\n")
+    # top100_list.append(movie_title[num].text
